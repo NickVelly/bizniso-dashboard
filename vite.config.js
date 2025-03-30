c98401821c64f4c9afa8282d1d655d9ea8c3e706
Nick Veličković <velickovicnikola97@yahoo.com>
2025-03-30 12:49:06 +0200

    Fix: Vercel build error with Firebase

---
+++ vite.config.js
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig(() => {
+  return {
+    base: './',
+    build: {
+      outDir: 'build',
+      rollupOptions: {
+        external: ['firebase'], // 🔧 OVDE rešavamo grešku sa Firebase
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [
+          autoprefixer(), // automatsko dodavanje vendor prefiksa
+        ],
+      },
+    },
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.jsx?$/,
+      exclude: [],
+    },
+    optimizeDeps: {
+      force: true,
+      esbuildOptions: {
+        loader: {
+          '.js': 'jsx',
+        },
+      },
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: 'src/',
+          replacement: `${path.resolve(__dirname, 'src')}/`,
+        },
+      ],
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        // Ako koristiš lokalni API server, dodaćeš ovde rutu
+        // primer: '/api': 'http://localhost:5000'
+      },
+    },
+  }
+})
